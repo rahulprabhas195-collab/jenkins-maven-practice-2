2c02233b88040f6df8d472e9a9be6392203464d0
root <root@ip-172-31-2-227.ap-south-1.compute.internal>
2026-09-21 10:07:24 +0000

    Initial calculator project

---
+++ src/main/java/com/example/Calculator.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int result = a + b;
+
+        System.out.println("Result = " + result);
+    }
+}
